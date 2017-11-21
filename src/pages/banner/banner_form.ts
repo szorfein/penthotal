ff03f7c0edc83e03aa2dae31b1227fa07ec1ec76
szorfein <szorfein@gmail.com>
2017-11-21 22:12:15 +0100

    base banner form

--- src/pages/banner/banner_form.ts
+++ src/pages/banner/banner_form.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core'
+import { AlertController } from 'ionic-angular'
+import { FormGroup , FormControl , Validators } from '@angular/forms'
 
 @Component({
+    selector: 'bannerform',
     templateUrl: 'banner_form.html'
 })
 
 export class BannerFormPage {
-    constructor() {}
+    form
+
+    constructor(private alertCtrl: AlertController) {
+        this.form = new FormGroup({
+            title: new FormControl("", Validators.required),
+            subtitle: new FormControl("", Validators.required),
+            image: new FormControl("", Validators.required)
+        })
+    }
+
+    doAlert() {
+        let alert = this.alertCtrl.create({
+            title: 'Edit banner',
+            message: '',
+            inputs: [
+                {
+                    name: this.form.title,
+                    placeholder: 'Title'
+                },
+                {
+                    name: this.form.subtitle,
+                    placeholder: 'Subtitle'
+                },
+                {
+                    name: this.form.image,
+                    placeholder: 'image.jpg'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked')
+                    }
+                },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        console.log('Saved clicked')
+                    }
+                }
+            ]
+        })
+        alert.present()
+    }
 
 }
