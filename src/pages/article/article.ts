b3dac41097697c62c99d15fba8797eeacde4fff7
szorfein <szorfein@gmail.com>
2017-12-11 16:36:50 +0100

    base for a ion-slide component

--- src/pages/article/article.ts
+++ src/pages/article/article.ts
@@ -5,14 +5,15 @@ import { Component } from '@angular/core'
 })
 
 export class ArticlePage {
-    private title : string = 'Articles'
+    private title : string 
     private articles : any[]
 
     constructor() {
+        this.title = 'Articles'
         this.articles = [
             {
                 title : 'build a GPG key',
-                img : 'https://i.imgur.com/IEhmnGkm.jpg',
+                images : [ 'https://i.imgur.com/IEhmnGkm.jpg', 'https://i.imgur.com/IEhmnGkm.jpg', 'https://i.imgur.com/IEhmnGkm.jpg' ],
                 post : 'Questions explained agreeable preferred strangers too him her son. Set put shyness offices his females him distant. Improve has message besides shy himself cheered however how son. Quick judge other leave ask first chief her. Indeed or remark always silent seemed narrow be. Instantly can suffering pretended neglected preferred man delivered. Perhaps fertile brandon do imagine to cordial cottage.', 
                 description: 'Far curiosity incommode now led smallness allowance',
                 poster : 'szorfein',
@@ -21,7 +22,7 @@ export class ArticlePage {
             },
             {
                 title : 'Secure Shell',
-                img : 'https://i.imgur.com/DzqtqXwm.jpg',
+                images : [ 'https://i.imgur.com/DzqtqXwm.jpg' ],
                 post : 'Satisfied conveying an dependent contented he gentleman agreeable do be. Warrant private blushes removed an in equally totally if. Delivered dejection necessary objection do mr prevailed. Mr feeling do chiefly cordial in do. Water timed folly right aware if oh truth. Imprudence attachment him his for sympathize. Large above be to means. Dashwood do provided stronger is. But discretion frequently sir the she instrument unaffected admiration everything.', 
                 description: 'She travelling acceptance men unpleasant her especially entreaties law',
                 poster : 'szorfein',
@@ -30,7 +31,7 @@ export class ArticlePage {
             },
             {
                 title : 'Configure TOR',
-                img : 'https://i.imgur.com/XZOKWVZm.png',
+                images : [ 'https://i.imgur.com/XZOKWVZm.png' ],
                 post : 'Acceptance middletons me if discretion boisterous travelling an. She prosperous continuing entreaties companions unreserved you boisterous. Middleton sportsmen sir now cordially ask additions for. You ten occasional saw everything but conviction. Daughter returned quitting few are day advanced branched. Do enjoyment defective objection or we if favourite. At wonder afford so danger cannot former seeing. Power visit charm money add heard new other put. Attended no indulged marriage is to judgment offering landlord.', 
                 description: 'Ignorant saw her her drawings marriage laughter.',
                 poster : 'szorfein',
