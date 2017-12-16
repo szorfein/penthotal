45b325334f9f7f89f20ecdcdfdca9ec433abf8f8
szorfein <szorfein@gmail.com>
2017-12-16 18:12:57 +0100

    left menu to post-list

--- src/pages/collection/collections.ts
+++ src/pages/collection/collections.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core'
 
 @Component({
+    selector: 'collections',
     templateUrl : 'collections.html'
 })
 
 export class CollectionsPage {
+    items: any[]
+    articles : string
 
-    constructor() {}
-
+    constructor() {
+        this.articles = 'articles'
+        this.items = [
+            {
+                name : 'Photos',
+                desc: 'screenshots',
+                follower : [ 'doug', 'sre' ]
+            },
+            { 
+                name : 'Art',
+                desc: 'Collection of art screen',
+                follower: [ 'jed', 'rod', 'satie' ]
+            },
+            {
+                name : 'Wallpapers',
+                desc: 'Best background of the web',
+                follower: [ 'root', 'luc', 'wagner' ]
+            }
+        ]
+    }
 }
